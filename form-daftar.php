36101e11a51e9b1d89e89902ebecadb679f0d22a
abbdhrs <146908608+abbdhrs@users.noreply.github.com>
2024-05-21 07:50:15 +0700

    Update form-daftar.php

--- form-daftar.php
+++ form-daftar.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Formulir Pendaftaran Siswa Baru | SMK Coding</title>
+    <title>Formulir Pendaftaran Siswa Baru | SMKN 2 PEKALONGAN</title>
 
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
